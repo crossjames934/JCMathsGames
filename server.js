7a8f723dac9aee7ed85d346802ada2cc88799cd4
crossjames934 <crossjames934@gmail.com>
2018-11-25 17:10:36 +0000

    server docs added

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const shortid = require('shortid');
+const hash = require('object-hash');
+
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+
+console.log("Short ID: " + shortid.generate());
